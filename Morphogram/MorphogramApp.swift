533dd4670cd8d57e9625ada33c0190570bc3eb27
Osman T <otufekci@bimser.com.tr>
2025-02-10 13:26:37 +0300

    Initial Commit

---
+++ Morphogram/MorphogramApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorphogramApp.swift
+//  Morphogram
+//
+//  Created by Osman Tufekci on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MorphogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
