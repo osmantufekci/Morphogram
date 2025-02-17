4f8fe993bad8c3a71467a4760085aa22667977f3
Osman T. <otufekci@bimser.com.tr>
2025-02-17 15:07:14 +0300

    UIImage Extension

---
+++ Morphogram/Exntensions/UIImage+Extensions.swift
@@ -0,0 +1,41 @@
+//
+//  UIImage+Extensions.swift
+//  Morphogram
+//
+//  Created by Osman Tufekci on 17.02.2025.
+//
+
+import SwiftUI
+
+extension UIImage {
+    func pixelBuffer(size: CGSize) -> CVPixelBuffer? {
+        var pixelBuffer: CVPixelBuffer?
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+                    kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        
+        let status = CVPixelBufferCreate(kCFAllocatorDefault,
+                                       Int(size.width),
+                                       Int(size.height),
+                                       kCVPixelFormatType_32ARGB,
+                                       attrs,
+                                       &pixelBuffer)
+        
+        guard status == kCVReturnSuccess, let buffer = pixelBuffer else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(buffer, CVPixelBufferLockFlags(rawValue: 0))
+        let context = CGContext(data: CVPixelBufferGetBaseAddress(buffer),
+                              width: Int(size.width),
+                              height: Int(size.height),
+                              bitsPerComponent: 8,
+                              bytesPerRow: CVPixelBufferGetBytesPerRow(buffer),
+                              space: CGColorSpaceCreateDeviceRGB(),
+                              bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.draw(self.cgImage!, in: CGRect(origin: .zero, size: size))
+        CVPixelBufferUnlockBaseAddress(buffer, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return buffer
+    }
+}
